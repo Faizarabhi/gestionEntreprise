ab24b1494d412d9011680097f4fdef7039e606cb
Faizarabhi <faizarabhi8@gmail.com>
2022-05-30 16:49:35 +0100

    update facteur clean

--- frontend/construct/src/components/CustomerComponents/facteur.vue
+++ frontend/construct/src/components/CustomerComponents/facteur.vue
@@ -1,107 +1,116 @@
-                <template>
-                <div class="bg-bg-bleu">
-                    <div class="grid grid-rows-2 place-content-center p-12">
-                    <div class="grid grid-cols-2 gap-24 place-content-center bg-white p-16">
-                        <div class="place-self-center">
-                        <img src="../../assets/images/logo.svg" />
-                        <p>Alex Fernández</p>
-                        <p>+6 49 62 05 35</p>
-                        <p>afsprodesign@gmail.com</p>
-                        </div>
-                        <div class="place-self-center">
-                        <h1 class="font-bold text-secondary">PRESUPUESTO</h1>
-                        <div class="">
-                            <h3 class="font-bold">Fetcha</h3>
-                            <p>{{this.date}}</p>
-                        </div>
-                        <div>
-                            <h3 class="font-bold">Descripción</h3>
-                            Rediseño y mejora UX-UI del site www.afsdesign.es
-                        </div>
-                        </div>
+                        <template>
+                        <div class="bg-bg-bleu">
+                            <div class="grid grid-rows-2 place-content-center p-12">
+                            <div class="grid grid-cols-2 gap-24 place-content-center bg-white p-16">
+                                <div class="place-self-center">
+                                <img src="../../assets/images/logo.svg" />
+                                <p>Alex Fernández </p>
+                                <p>+6 49 62 05 35</p>
+                                <p>afsprodesign@gmail.com</p>
+                                </div>
+                                <div class="place-self-center">
+                                <h1 class="font-bold text-secondary">PRESUPUESTO</h1>
+                                <div class="">
+                                    <h3 class="font-bold">Fetcha</h3>
+                                    <p>{{this.date}}</p>
+                                </div>
+                                <div>
+                                    <h3 class="font-bold">Descripción</h3>
+                                    <p> Rediseño y mejora UX-UI del site www.afsdesign.es</p>
 
-                        <div class="place-self-center col-span-2">
-                        <button @click="addrow()">add row</button>
+                                </div>
+            
+                                </div>
 
-                        <table class="table table-compact">
-                            <thead>
-                        <tr>
-                            <th>
-                            <td></td>
-                            <td>Categorie</td>
-                            <td>Produit</td>
-                            <td>Quantité</td>
-                            <td>Unité</td>
-                            <td>Prix Unitaire HT</td>
-                            <td>Prix Total HT</td></th>
-                        </tr>
-                        </thead>
-                            
-                            
-                            <tbody>
-                            <tr v-for="i in add" :key="i">
-                                <rowfacteur :i="i"/>
-                            </tr>
-                            </tbody>
-                            <tfoot>
-                            
-                        <tr>
-                        
-                            <th>
-                            <td></td>
-                            <td>Categorie</td>
-                            <td>Produit</td>
-                            <td>Quantité</td>
-                            <td>Unité</td>
-                            <td>Prix Unitaire HT</td>
-                            <td>Prix Total HT</td>
-                            </th>
-                        </tr>
-                        
-                            </tfoot>
-                        </table>
-                        
+                                    <p>{{id_fact}}</p>
+                                <div class="place-self-center col-span-2">
+                                <button @click="addrow()">add row</button>
+
+                                <table class="table table-compact">
+                                    <thead>
+                                <tr>
+                                    <th>
+                                    <td></td>
+                                    <td>Categorie</td>
+                                    <td>Produit</td>
+                                    <td>Quantité</td>
+                                    <td>Unité</td>
+                                    <td>Prix Unitaire HT</td>
+                                    <td>Prix Total HT</td></th>
+                                </tr>
+                                </thead>
+                                    
+                                    
+                                    <tbody>
+                                    <tr v-for="command in data.list" :key="command.id">
+                                        <rowfacteur :command="command"/>
+                                    </tr>
+                                    </tbody>
+                                    <tfoot>
+                                    
+                                <tr>
+                                
+                                    <th>
+                                    <td></td>
+                                    <td>Categorie</td>
+                                    <td>Produit</td>
+                                    <td>Quantité</td>
+                                    <td>Unité</td>
+                                    <td>Prix Unitaire HT</td>
+                                    <td>Prix Total HT</td>
+                                    </th>
+                                </tr>
+                                
+                                    </tfoot>
+                                </table>
+                                
+                                </div>
+                            </div>
+                            </div>
                         </div>
-                    </div>
-                    </div>
-                </div>
-                </template>
-                <script>
-                import router from "@/router";
-                import rowfacteur from "../CustomerComponents/rowfacteur.vue";
-                import {computed} from"vue";
+                        </template>
+                        <script>
+    import router from "@/router";
+    import rowfacteur from "../CustomerComponents/rowfacteur.vue";
+    import { computed } from "vue";
 
-                export default {
-                components: { rowfacteur },
-                provide(){
-                    return {
-                        addCmd: this.addCmd,
-                        updateCmd: this.updateCmd,
-                        removeCmd: this.removeCmd,
-                        categorie: computed(() => this.categorie),
-                        product: computed(() => this.product)
-                    }
-                },
-                data(){
-                    return{
-                        add: 1,
-                        date : "",
-                        data: {
-                            list: []
-                        },
-                        categorie: [],
-                        product: [],
-                        } 
-                }, 
-                mounted() {
-                    this.getAll_categorie();
-                    this.date_function(),
-                    // console.log("idcustomer"),
-                    console.log(this.$cookies.get("idcustomer"))
-                },
-                methods: {
-                    async getAllProduct(event) {
-        // console.log("hhh");
+    export default {
+    components: { rowfacteur },
+    inject: ["id_fact"],
+    provide() {
+        return {
+        addCmd: this.addCmd,
+        updateCmd: this.updateCmd,
+        removeCmd: this.removeCmd,
+        categorie: computed(() => this.categorie),
+        product: computed(() => this.product),
+        };
+    },
+
+    data() {
+        return {
+        add: 1,
+        date: "",
+        // data katjm3 hna kolha katruturna hna (id facteur) list katruturni tabl=>2 feh les cmds
+        data: {
+            id: this.getId(),
+            list: [],
+        },
+        categorie: [],
+        product: [],
+        };
+    },
+    mounted() {
+        this.getAll_categorie();
+        this.date_function(), console.log(this.$cookies.get("idcustomer"));
+    },
+    methods: {
+        getId() {
+        return (
+            Math.random().toString(36).slice(2) + new Date().getTime().toString(36)
+        );
+        },
+        async getAllProduct(event) {
         this.categ = event.target.value;
         let res = await fetch(
             "http://localhost/filrouge/backend/public/ProductController/get_productBycategorie",
@@ -116,46 +125,47 @@
             }
         );
         this.product = await res.json();
-        // console.log(this.product);
         },
-                    async getAll_categorie() {
-                        let respons = await fetch(
-                            "http://localhost/filrouge/backend/public/CategorieController/getAll_categorie"
+
+        async getAll_categorie() {
+        let respons = await fetch(
+            "http://localhost/filrouge/backend/public/CategorieController/getAll_categorie"
         );
         this.categorie = await respons.json();
         // this.getAllProduct(id_categorie);
         },
-                    addCmd(cmd){
-                        this.data.list.push({...cmd, id: this.data.list.length})
-                        console.log(this.data.list);
-                    },
-                    updateCmd(index, updates){
-                        this.data.list = this.data.list.map((c, i) => {
-                            if(i === index){
-                                return {...c, ...updates};
-                            }
-                            return c;
-                        })
-                    },
-                    removeCmd(index){
-                        this.data.list = this.data.list.filter((cmd, i) =>  i !== index);
-                    },
-                    addrow() {
-                    this.add = this.add + 1;
-                    console.log(this.add)
-                    },
-                    date_function() {
-            
-                        
-                        var formatted_date = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-                        this.date =formatted_date ;
-                        console.log(this.date)
-                
-                    },
-                    
-                    
-                    
-                },
-                
-                };
-                </script>
+        addCmd(cmd) {
+        this.data.list.push({ ...cmd, id: this.data.list.length });
+        console.log(this.data.list);
+        },
+        updateCmd(id, updates) {
+        this.data.list = this.data.list.map((c, i) => {
+            if (c.id === id) {
+            return { ...c, ...updates };
+            }
+            return c;
+        });
+        },
+        removeCmd(id) {
+        this.data.list = this.data.list.filter((cmd, i) => cmd.id !== id);
+        },
+        addrow() {
+        // this.add = this.add + 1;
+        // console.log(this.add)
+        const data = {
+            id: this.getId(),
+            product_id: "",
+            quantity: 1,
+            prix: 0,
+        };
+        //   console.log(data);
+        this.data.list.push(data);
+        },
+        date_function() {
+        var formatted_date = new Date().toJSON().slice(0, 10).replace(/-/g, "/");
+        this.date = formatted_date;
+        console.log(this.date);
+        },
+    },
+    };
+    </script>
--- frontend/construct/src/components/CustomerComponents/rowfacteur.vue
+++ frontend/construct/src/components/CustomerComponents/rowfacteur.vue
@@ -1,121 +1,90 @@
-    <template>
-    
-        <th>
-            <td>{{ i }}</td>
-        <td>
-        <select
-            @change="getAllProduct($event)"
-            name=""
-            class="select select-bordered max-w-xs"
-        >
-            <option disabled selected>Categorie</option>
-            <option v-for="(cat, i) in categorie?.value" :key="i" :value="cat?.id">
-            {{ cat?.name }}
-            </option>
-        </select>
-        </td>
-        <td>
-        <select v-model="infoproduct.productid"
-        
-            @change="getproduct($event)"
-            class="select select-bordered max-w-xs"
-        >
-            <option disabled selected>Product</option>
-            <option v-for="(pro, i) in product?.value" :key="i" :value="pro?.id">
-            {{ pro?.ref_prdt }}
-            </option>
-        </select>
-        </td>
-        <td>
-        <input
-            @change="prixTotal($event)"
-            type="number"
-            placeholder="Type here"
-            class="input input-bordered input-md max-w-xs"
-        />
-        </td>
-        <td><input v-model="infoproduct.unite" disabled /></td>
-        <td>{{ infoproduct.prixunitaire }}</td>
-        <td>{{ infoproduct.prixtotal }}</td>
-        <td  @click="addcmd">add cmd</td>
-        </th>
-        
-    </template>
-
-    <script>
-    import { v4 as uuidv4 } from 'uuid'
-    export default {
-    props: {
-        i: Number,
-    },
-    inject:["addCmd", "removeCmd",'updateCmd',"categorie"],
-    
-    data() {
-        return {
+            <template>
             
-        categ: 0,
-        prod: 0,
-        facturform:{
-            idfact : "",
-            idclient : this.$cookies.get("idcustomer"),
+                <th>
+                <td>
+                <select
+                    @change="getAllProduct($event)"
+                    name=""
+                    class="select select-bordered max-w-xs"
+                >
+                    <option disabled selected>Categorie</option>
+                    <option v-for="(cat, i) in categorie?.value" :key="i" :value="cat?.id">
+                    {{ cat?.name }}
+                    </option>
+                </select>
+                </td>
+                <td>
+                <select 
+                    v-model="product"
+                    class="select select-bordered max-w-xs"
+                >
+                    <option disabled selected>Product</option>
+                    <option v-for="product in products" :key="product.id" :value="product"
+                    >
+                    {{ product.ref_prdt }}
+                    </option>
+                </select>
+                </td>
+                <td>
+                <input
+                    v-model="quantity"
+                    type="number"
+                    placeholder="Type here"
+                    class="input input-bordered input-md max-w-xs"
+                />
+                </td>
+                <td>prix {{prix}}</td>
+                <!-- <td>{{ infoproduct.prixunitaire }}</td>
+                <td>{{ infoproduct.prixtotal }}</td> -->
+                <td  @click="removeCmd(command.id)">remove cmd</td> 
+                </th>
+                
+            </template>
 
+            <script>
+        import { computed } from "vue";
+        export default {
+        props: ["command"],
+        inject: ["removeCmd", "updateCmd", "categorie"],
+        data() {
+            return {
+            category: undefined,
+            products: [],
+            product: undefined,
+            quantity: this.command.quantity,
+            prix: computed(() => this.quantity * this.command.prix),
+            };
         },
-        infoproduct: {
-            idform: "",
-            unite: "",
-            productid : "",
-            prixunitaire: "0",
-            prixtotal: "0",
-            multi: "",
-        },
-        infoarr : {"":  ""},
-
-        multiple: "",
-        info: {},
-        };
-    },
-    mounted() {
-        this.facturform.idfact = uuidv4();
-        console.log(this.facturform.idfact)
-        console.log(this.facturform);
-    },
-    methods: {
-        
-        
-        async getproduct(event) {
-        this.prod = event.target.value;
-        // console.log(this.prod)
-        // http://localhost/filrouge/backend/public/ProductController/get_product
-        let resp = await fetch(
-            "http://localhost/filrouge/backend/public/ProductController/get_product",
-            {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
+        watch: {
+            product() {
+            this.updateCmd(this.command.id, {
+                product_id: this.product.id,
+                prix: this.product.prix_unitaire,
+            });
             },
-            body: JSON.stringify({
-                id: this.prod,
-            }),
-            }
-        );
-        this.info = await resp.json();
-        this.infoproduct.unite = this.info.unite;
-        // this.infoproduct.id = this.i;
-        this.infoproduct.prixunitaire = this.info.prix_unitaire;
-        this.multiple = this.infoproduct.multi;
-
-        console.log(this.infoproduct.prixunitaire);
+            quantity() {
+            this.updateCmd(this.command.id, {
+                quantity: this.quantity,
+            });
+            },
         },
-        prixTotal(event) {
-        this.infoproduct.prixtotal = this.info.prix_unitaire * event.target.value;
-        // console.log(event.target.value);
-        
+        methods: {
+            async getAllProduct(event) {
+            this.category = event.target.value;
+            let res = await fetch(
+                "http://localhost/filrouge/backend/public/ProductController/get_productBycategorie",
+                {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    id: this.category,
+                }),
+                }
+            );
+            this.products = await res.json();
+            },
         },
-        addcmd(){
-            this.infoproduct.idform = this.i ;
-            this.infoarr = this.infoproduct 
-            this.addCmd(this.infoarr)
-        }
-    },
-    };
-    </script>
+        };
+        </script>
